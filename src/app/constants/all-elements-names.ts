c0fde0c90ffd80730339c154b575bc2ef190e449
Arunkumar velupula <srilatha.shirisha@gmail.com>
2021-04-26 16:36:50 -0400

    Added css

--- src/app/constants/all-elements-names.ts
+++ src/app/constants/all-elements-names.ts
@@ -7,5 +7,7 @@ export const checkBoxes = [
     { id: 'facility_002', value: 'Facility 002', name: 'facility_002', isChecked: false },
     { id: 'facility_003', value: 'Facility 003', name: 'facility_003', isChecked: false },
     { id: 'facility_004', value: 'Facility 004', name: 'facility_004', isChecked: false },
-    { id: 'facility_005', value: 'Facility 005', name: 'facility_005', isChecked: false }
+    { id: 'facility_005', value: 'Facility 005', name: 'facility_005', isChecked: false },
+    { id: 'facility_006', value: 'Facility 006', name: 'facility_006', isChecked: false },
+    { id: 'facility_007', value: 'Facility 007', name: 'facility_007', isChecked: false },
 ];
